571d8800eaa1332f9aef0761e0c3ca1d3a065a68
SuneetR <suneet38459@gmail.com>
2025-01-05 10:12:14 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Carousel Animation
+const track = document.querySelector('.carousel-track');
+let currentIndex = 0;
+
+function moveCarousel() {
+    currentIndex += 1;
+    const items = track.children.length;
+
+    if (currentIndex >= items) {
+        currentIndex = 0;
+    }
+    track.style.transform = `translateX(-${currentIndex * 320}px)`;
+}
+
+setInterval(moveCarousel, 3000);
